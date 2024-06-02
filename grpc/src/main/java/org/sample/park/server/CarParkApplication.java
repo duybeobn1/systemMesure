924ab2741859a7c6063141bec11e2ab1d305e0d6
duybeobn1 <duybeobn1@gmail.com>
2024-06-02 20:47:01 +0700

    .gitignore

--- grpc/src/main/java/org/sample/park/server/CarParkApplication.java
+++ grpc/src/main/java/org/sample/park/server/CarParkApplication.java
@@ -2,15 +2,10 @@ package org.sample.park.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "org.sample.park.client")
-@ComponentScan(basePackages = "org.sample.park")
 @EnableJpaRepositories(basePackages = "org.sample.park.repository")
-
 public class CarParkApplication {
     public static void main(String[] args) {
         SpringApplication.run(CarParkApplication.class, args);
